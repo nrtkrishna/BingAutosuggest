2430aa45c3a4efdf0b806c224396be26341ca5f1
Krishna Tula <nrt.krishna@gmail.com>
2018-12-10 13:31:34 +0530

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+
+// <reference  path="../../node_modules\bingmaps\types\MicrosoftMaps\Modules\AutoSuggest.d.ts"/>
+/// <reference path="../../node_modules/bingmaps/types/MicrosoftMaps/Microsoft.Maps.All.d.ts" />
+// <reference  path="../../node_modules\bingmaps\types\MicrosoftMaps\Modules\SpatialMath.d.ts"/>
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
