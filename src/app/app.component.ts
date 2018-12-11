91707f5dcace237fd32cb952eb7f8846444bf83e
Krishna Tula <nrt.krishna@gmail.com>
2018-12-11 22:07:20 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Inject, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Inject, AfterViewInit, ElementRef, NgZone } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -9,17 +9,16 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   title = 'AutosuggestWithMap';
+  @ViewChild('searchBox') private searchBox: ElementRef;
+  @ViewChild('searchBoxContainer') private searchBoxContainer: ElementRef;
+  @ViewChild('myMap') private myMap: ElementRef;
 
   // constructor(@Inject(DOCUMENT) private document, private elementRef: ElementRef) { }
-  constructor() { }
+  constructor(private ngZone: NgZone) { }
 
   ngOnInit(){
-    var self = this;
-    Microsoft.Maps.loadModule('Microsoft.Maps.Autosuggest', () => {
-      var options = {maxResults: 5}; 
-      var manager = new Microsoft.Maps.AutosuggestManager(options);
-      manager.attachAutosuggest('#searchBox', '#searchBoxContainer', self.selectedSuggestion);
-    });
+  
+
   }
 
   selectedSuggestion(suggestionResult) {
@@ -28,6 +27,21 @@ export class AppComponent implements OnInit {
 
   ngAfterViewInit() {
 
+    this.ngZone.run(() => {
+      setTimeout (() => {
+       var map:any = new Microsoft.Maps.Map('#myMap', {
+          credentials: 'AktQDKefhl630pH0Dfe5TQJ6GRUDoT16CHPs0BNpBBJxq1u7LBlJOEdhpzQ4TN3_'
+      });
+
+        Microsoft.Maps.loadModule('Microsoft.Maps.AutoSuggest', () => {
+          // var options = {maxResults: 5}; 
+          var manager = new Microsoft.Maps.AutosuggestManager({ map: map });
+          manager.attachAutosuggest('#searchBox', '#searchBoxContainer', this.selectedSuggestion);
+        });
+      }, 1000);
+      
+        
+  });
 
 }
 }
